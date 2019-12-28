e9a0d5f431cfecb9f2ba8a92b5a94a6ec0948082
Anuken <arnukren@gmail.com>
2019-12-28 18:57:07 -0500

    Added base files

---
+++ src/main/java/hexed/Generator.java
@@ -0,0 +1,4 @@
+package hexed;
+
+public class Generator{
+}
---
+++ src/main/java/hexed/HexedPlugin.java
@@ -0,0 +1,22 @@
+package hexed;
+
+import arc.util.*;
+import mindustry.plugin.*;
+
+public class HexedPlugin extends Plugin{
+
+    @Override
+    public void init(){
+
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler){
+
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+
+    }
+}
