5e391f1cd06284674d2817fcd7c56d3f8243fc6a
Recessive <61154659+Recessive@users.noreply.github.com>
2020-03-05 01:32:41 +1100

    Added c**t to swear filter

--- src/main/java/hexed/CurseFilter.java
+++ src/main/java/hexed/CurseFilter.java
@@ -3,5 +3,5 @@ package hexed;
 //TODO put in other plugin
 public class CurseFilter{
     //this is probably the most curses I've ever typed
-    public static final String[] swears = {"shit", "fuck", "asshole", "nigger", "nigga", "faggot", "dick", "bitch"};
+    public static final String[] swears = {"shit", "fuck", "asshole", "nigger", "nigga", "niga", "faggot", "dick", "cunt", "bitch"};
 }
